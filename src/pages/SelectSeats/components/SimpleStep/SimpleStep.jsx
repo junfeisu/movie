7ee5c0bf116d2402281d31a2df90fc14bc0cabae
junfeisu <1982764150@qq.com>
2018-04-27 19:43:12 +0800

    add simpleStep

--- src/pages/SelectSeats/components/SimpleStep/SimpleStep.jsx
+++ src/pages/SelectSeats/components/SimpleStep/SimpleStep.jsx
@@ -19,39 +19,15 @@ export default class SimpleStep extends Component {
     };
   }
 
-  next = () => {
-    const s = this.state.currentStep + 1;
-
-    this.setState({
-      currentStep: s > 6 ? 6 : s,
-    });
-  };
-
-  prev = () => {
-    const s = this.state.currentStep - 1;
-
-    this.setState({
-      currentStep: s < 0 ? 0 : s,
-    });
-  };
-
-  onClick = (currentStep) => {
-    console.log(currentStep);
-
-    this.setState({
-      currentStep,
-    });
-  };
-
   render() {
     const { currentStep } = this.state;
 
     return (
       <IceContainer>
         <Step current={currentStep}>
-          <StepItem title="选择影院，场次" onClick={this.onClick} />
-          <StepItem title="选座，填写手机号" onClick={this.onClick} />
-          <StepItem title="确认订单" onClick={this.onClick} />
+          <StepItem title="选择影院，场次" />
+          <StepItem title="选座，填写手机号" />
+          <StepItem title="确认订单" />
         </Step>
       </IceContainer>
     );
